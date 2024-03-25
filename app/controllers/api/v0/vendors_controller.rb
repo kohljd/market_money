69151aeb3372bea6e5d2499d2970286f020eb43d
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-25 17:14:40 -0500

    style: tabs

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,11 +1,11 @@
 class Api::V0::VendorsController < ApplicationController  
-    # def show
-    #    render json: Vendor.find(params[:id])
-    # end
-    def show
-        vendor = Vendor.find(params[:id])
-        render json: vendor
-      rescue ActiveRecord::RecordNotFound
-        render json: { errors: [{ detail: "Vendor with id=#{params[:id]} not found" }] }, status: :not_found
-      end
+  # def show
+  #    render json: Vendor.find(params[:id])
+  # end
+  def show
+    vendor = Vendor.find(params[:id])
+    render json: vendor
+    rescue ActiveRecord::RecordNotFound
+      render json: { errors: [{ detail: "Vendor with id=#{params[:id]} not found" }] }, status: :not_found
   end
+end
