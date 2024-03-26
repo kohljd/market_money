bb21d025128aa74e9c523ce0d8b7dab860e6eefc
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-26 17:49:04 -0500

    test: get market by id - valid

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -51,4 +51,51 @@ describe "Market API" do
       expect(market[:attributes][:vendor_count]).to be_an(Integer)
     end
   end
+
+  it "can get a market by its id" do
+    market_id = create(:market).id
+
+    get "/api/v0/markets/#{id}"
+
+    expect(response).to be_successful
+
+    formatted_response = JSON.parse(response.body, symbolize_names: true)
+    market = formatted_response[:data]
+
+    expect(market).to have_key(:id)
+    expect(market[:id]).to be_an(Integer)
+
+    expect(market).to have_key(:type)
+    expect(market[:type]).to be_a(String)
+
+    expect(market).to have_key(:attributes)
+    expect(market[:attributes]).to be_a(Hash)
+
+    expect(market[:attributes]).to have_key(:name)
+    expect(market[:attributes][:name]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:street)
+    expect(market[:attributes][:street]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:city)
+    expect(market[:attributes][:city]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:county)
+    expect(market[:attributes][:county]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:state)
+    expect(market[:attributes][:state]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:zip)
+    expect(market[:attributes][:zip]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:lat)
+    expect(market[:attributes][:lat]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:lon)
+    expect(market[:attributes][:lon]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:vendor_count)
+    expect(market[:attributes][:vendor_count]).to be_an(Integer)
+  end
 end
