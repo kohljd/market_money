219779b9f44c3e2fb3391a07dae321fddba9c450
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-25 20:16:47 -0500

    test: market's vendor count

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Market, type: :model do
     it { should have_many :market_vendors }
     it { should have_many(:vendors).through(:market_vendors) }
   end
+
+  describe "instance methods" do
+    describe ".vendor_count" do
+      it "counts a market's vendors" do
+        market_1 = create(:market)
+        vendor_1 = create(:vendor)
+        vendor_2 = create(:vendor)
+        market_vendors = MarketVendor.create!(market: market_1, vendor: vendor_1)
+        market_vendors = MarketVendor.create!(market: market_1, vendor: vendor_2)
+
+        expect(market_1.vendor_count).to eq(2)
+
+        market_2 = create(:market)
+        expect(market_2.vendor_count).to eq(0)
+      end
+    end
+  end
 end
