8d7e6fe9a5db3640685123d1646347d899fef5d7
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-27 01:41:38 -0500

    refactor market api formatting tests

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -10,92 +10,46 @@ describe "Market API" do
 
     formatted_response = JSON.parse(response.body, symbolize_names: true)
     markets = formatted_response[:data]
-    
+
     expect(markets.count).to eq(5)
 
     markets.each do |market|
-      expect(market).to have_key(:id)
-      expect(market[:id]).to be_an(Integer)
+      expect(market).to include(:id, :type, :attributes)
 
-      expect(market).to have_key(:type)
+      expect(market[:id]).to be_a(String)
       expect(market[:type]).to be_a(String)
-
-      expect(market).to have_key(:attributes)
       expect(market[:attributes]).to be_a(Hash)
+      
+      expect(market[:attributes]).to include(
+        :name, :street, :city, :county, :state, :zip, :lat, :lon, :vendor_count
+      )
 
-      expect(market[:attributes]).to have_key(:name)
-      expect(market[:attributes][:name]).to be_a(String)
-
-      expect(market[:attributes]).to have_key(:street)
-      expect(market[:attributes][:street]).to be_a(String)
-
-      expect(market[:attributes]).to have_key(:city)
-      expect(market[:attributes][:city]).to be_a(String)
-
-      expect(market[:attributes]).to have_key(:county)
-      expect(market[:attributes][:county]).to be_a(String)
-
-      expect(market[:attributes]).to have_key(:state)
-      expect(market[:attributes][:state]).to be_a(String)
-
-      expect(market[:attributes]).to have_key(:zip)
-      expect(market[:attributes][:zip]).to be_a(String)
-
-      expect(market[:attributes]).to have_key(:lat)
-      expect(market[:attributes][:lat]).to be_a(String)
-
-      expect(market[:attributes]).to have_key(:lon)
-      expect(market[:attributes][:lon]).to be_a(String)
-
-      expect(market[:attributes]).to have_key(:vendor_count)
+      expect(market[:attributes].except(:vendor_count).values).to all(be_a(String))
       expect(market[:attributes][:vendor_count]).to be_an(Integer)
     end
   end
 
   it "can get a market by its id" do
-    market_id = create(:market).id
+    market_1 = create(:market)
 
-    get "/api/v0/markets/#{id}"
+    get "/api/v0/markets/#{market_1.id}"
 
     expect(response).to be_successful
 
     formatted_response = JSON.parse(response.body, symbolize_names: true)
     market = formatted_response[:data]
 
-    expect(market).to have_key(:id)
-    expect(market[:id]).to be_an(Integer)
+    expect(market).to include(:id, :type, :attributes)
 
-    expect(market).to have_key(:type)
+    expect(market[:id]).to be_a(String)
     expect(market[:type]).to be_a(String)
-
-    expect(market).to have_key(:attributes)
     expect(market[:attributes]).to be_a(Hash)
-
-    expect(market[:attributes]).to have_key(:name)
-    expect(market[:attributes][:name]).to be_a(String)
-
-    expect(market[:attributes]).to have_key(:street)
-    expect(market[:attributes][:street]).to be_a(String)
-
-    expect(market[:attributes]).to have_key(:city)
-    expect(market[:attributes][:city]).to be_a(String)
-
-    expect(market[:attributes]).to have_key(:county)
-    expect(market[:attributes][:county]).to be_a(String)
-
-    expect(market[:attributes]).to have_key(:state)
-    expect(market[:attributes][:state]).to be_a(String)
-
-    expect(market[:attributes]).to have_key(:zip)
-    expect(market[:attributes][:zip]).to be_a(String)
-
-    expect(market[:attributes]).to have_key(:lat)
-    expect(market[:attributes][:lat]).to be_a(String)
-
-    expect(market[:attributes]).to have_key(:lon)
-    expect(market[:attributes][:lon]).to be_a(String)
-
-    expect(market[:attributes]).to have_key(:vendor_count)
+    
+    expect(market[:attributes]).to include(
+      :name, :street, :city, :county, :state, :zip, :lat, :lon, :vendor_count
+    )
+    
+    expect(market[:attributes].except(:vendor_count).values).to all(be_a(String))
     expect(market[:attributes][:vendor_count]).to be_an(Integer)
   end
 
@@ -107,7 +61,6 @@ describe "Market API" do
 
     formatted_response = JSON.parse(response.body, symbolize_names: true)
     error_response = formatted_response[:errors]
-
     expect(error_response).to be_an(Array)
     expect(error_response.first).to have_key(:detail)
     expect(error_response.first[:detail]).to start_with("Couldn't find Market with 'id'=")
