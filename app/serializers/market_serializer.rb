180342ce8e487d1c205d68ea696338ed07ae66ed
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-26 20:45:53 -0500

    update market serializer to use JSONAPI gem

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,23 +1,4 @@
 class MarketSerializer
-  def self.format_markets(markets)
-    {
-      data: markets.map do |market|
-        {
-          id: market.id,
-          type: "market",
-          attributes: {
-            name: market.name,
-            street: market.street,
-            city: market.city,
-            county: market.county,
-            state: market.state,
-            zip: market.zip,
-            lat: market.lat,
-            lon: market.lon,
-            vendor_count: market.vendor_count
-          }
-        }
-      end
-    }
-  end
+  include JSONAPI::Serializer
+  attributes :name, :street, :city, :county, :state, :zip, :lat, :lon, :vendor_count
 end
