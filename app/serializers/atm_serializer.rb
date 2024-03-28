7a5a29122cd96a68a0af3702cdccf2e8728ff7ea
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-28 18:35:35 -0500

    change to be handrolled due to id issue

--- app/serializers/atm_serializer.rb
+++ app/serializers/atm_serializer.rb
@@ -1,4 +1,19 @@
 class AtmSerializer
-  include JSONAPI::Serializer
-  attributes :name, :address, :lat, :lon, :distance
+  def self.format_atms(atms)
+    {
+      data: atms.map do |atm|
+        {
+          id: "null",
+          type: "atm",
+          attributes: {
+              name: atm.name,
+              address: atm.address,
+              lat: atm.lat,
+              lon: atm.lon,
+              distance: atm.distance
+          }
+        }
+      end
+    }
+  end
 end
